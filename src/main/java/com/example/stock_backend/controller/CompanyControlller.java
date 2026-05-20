6afedf39b1b1372ae38a92b1d49eee617b5870f4
Shankar Narayan <shankarnarayan1555@gmail.com>
2026-05-20 15:42:59 +0200

    update changes

--- src/main/java/com/example/stock_backend/controller/CompanyControlller.java
+++ src/main/java/com/example/stock_backend/controller/CompanyControlller.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,6 @@ import com.example.stock_backend.model.Company;
 import com.example.stock_backend.service.CompanyService; 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
 public class CompanyControlller {
     
     @Autowired 
